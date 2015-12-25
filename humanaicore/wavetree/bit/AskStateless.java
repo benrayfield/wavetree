8748a9072620ad8540bb56eaf8c5bb1bdee70f23
Ben F Rayfield <ben@humanai.net>
2015-12-25 18:26:39 -0500

    Revert "Wavetree 0.2.1"
    This reverts commit 7b30a111c9cb8aa06a5ec004da6d1380ee93e843.

--- humanaicore/wavetree/bit/AskStateless.java
+++
@@ -1,8 +0,0 @@
-/** Ben F Rayfield offers HumanAiCore opensource GNU LGPL */
-package humanaicore.wavetree.bit;
-
-public interface AskStateless{
-	
-	public boolean isStateless();
-
-}
--- humanaicore/wavetree/bit/BitFunc.java
+++
@@ -1,8 +0,0 @@
-/** Ben F Rayfield offers HumanAiCore opensource GNU LGPL */
-package humanaicore.wavetree.bit;
-
-public interface BitFunc extends AskStateless{
-	
-	public Bits bitfunc(Bits in);
-
-}
